cb45a8f52e6c225aba2020c8d79812ba7e166e75
Aimee Rivers <airi@dr.dk>
2021-01-22 00:55:21 +0100

    Changes needed for UK map style.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let map = document.getElementById("map");
 let mapBox = getMapBoundingBox(regions);
-map.style.width = multiplier * (1 + mapBox[1][0] - mapBox[0][0]) + "px";
-map.style.height = multiplier * (1 + mapBox[1][1] - mapBox[0][1]) + "px";
+map.style.width = xMultiplier * (1 + mapBox[1][0] - mapBox[0][0]) + "px";
+map.style.height = yMultiplier * (1 + mapBox[1][1] - mapBox[0][1]) + "px";
 
 regions.forEach(region => {
   let areaNumber = 0;
@@ -10,9 +10,9 @@ regions.forEach(region => {
     area.coordinates.forEach(coordinate => {
       blob = document.createElement("span");
       blob.classList.add("blob");
-      blob.style.left = multiplier * coordinate[0] + "px";
-      blob.style.top = multiplier * coordinate[1] + "px";
       blob.classList.add(slugify(region.name, areaNumber));
+      blob.style.left = xMultiplier * coordinate[0] + "px";
+      blob.style.top = yMultiplier * coordinate[1] + "px";
       if(area.result) {
         blob.classList.add(area.result);
       }
@@ -24,11 +24,11 @@ regions.forEach(region => {
   label.classList.add("region-label");
   label.classList.add(slugify(region.name));
   label.innerText = region.short;
-  label.style.left = multiplier * box[0][0] + "px";
-  label.style.top = multiplier * box[0][1] + "px";
-  label.style.width = multiplier * (1 + box[1][0] - box[0][0]) + "px";
-  label.style.height = multiplier * (1 + box[1][1] - box[0][1]) + "px";
-  label.style.lineHeight = multiplier * (1 + box[1][1] - box[0][1]) + "px";
+  label.style.left = xMultiplier * box[0][0] + "px";
+  label.style.top = yMultiplier * box[0][1] + "px";
+  label.style.width = xMultiplier * (1 + box[1][0] - box[0][0]) + "px";
+  label.style.height = yMultiplier * (1 + box[1][1] - box[0][1]) + "px";
+  label.style.lineHeight = yMultiplier * (1 + box[1][1] - box[0][1]) + "px";
   map.appendChild(label);
 });
 
